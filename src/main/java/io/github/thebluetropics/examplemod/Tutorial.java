cc1da25d24aebbd6cdbc3059441566a7f74f498b
thebluetropics <thebluetropics@gmail.com>
2024-12-06 23:55:51 +0700

    add Tutorial class

---
+++ src/main/java/io/github/thebluetropics/examplemod/Tutorial.java
@@ -0,0 +1,16 @@
+package io.github.thebluetropics.examplemod;
+
+public class Tutorial {
+
+  public int myVal = 0;
+
+  /// example empty function
+  public void init() {
+    // empty...
+  }
+
+  /// example function with custom return type
+  public boolean myFunc1() {
+    return true;
+  }
+}
